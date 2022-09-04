b4741f521bd2c8c46679ce1c65350938146ac921
Leandro Dias <ldias@processwaregroup.com>
2022-09-04 10:06:42 -0300

    Criação dos models
    Criação dos models

---
+++ Todo.API/Models/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Todo.API.Models
+{
+    public class BaseModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime InsertedAt { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Todo.API/Models/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace Todo.API.Models
+{
+    public class Todo : BaseModel
+    {
+        public string Name { get; set; }
+        public bool Completed { get; set; }
+    }
+}
