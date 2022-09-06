1b7ff5d7375994431e421285a5721d21c1662470
Leandro Dias <ldias@processwaregroup.com>
2022-09-06 07:34:18 -0300

    Configuração docker-compose
    Configuração docker-compose

--- Todo.API/Program.cs
+++ Todo.API/Program.cs
@@ -17,12 +17,18 @@ builder.Host.ConfigureLogging(logging =>
 var configuration = builder.Configuration;
 builder.Services.AddEntityFrameworkNpgsql()
              .AddDbContext<DataContext>(options => options.UseNpgsql(configuration.GetConnectionString("DbConnection")));
+
+var dataContext = builder.Services.BuildServiceProvider().GetService<DataContext>();
+if (dataContext != null)
+{
+    dataContext.Database.Migrate();
+}
+
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 builder.Services.AddTransient<ITodoRepository, TodoRepository>();
 builder.Services.AddTransient<IAuditService, AuditService>();
 
 builder.Services.AddGrpcClient<AuditProtoService.AuditProtoServiceClient>(opts => opts.Address = new Uri(configuration["GrpcSettings:AuditUrl"]));
-;
 
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -35,12 +41,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
